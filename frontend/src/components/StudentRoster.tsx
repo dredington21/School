75ef204404ddeb35f88ac337d269d729195d3908
madelinevz <149022917+madelinevz@users.noreply.github.com>
2024-11-24 22:38:33 -0500

    student roster front end changes

--- frontend/src/components/StudentRoster.tsx
+++ frontend/src/components/StudentRoster.tsx
@@ -1,18 +1,65 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Accordion } from "react-bootstrap";
-import { TestMajors, TestStudents } from "../testData";
-import { Student } from "../types";
+
+interface RosterStudent {
+  major_name: string;
+  student_id: string;
+  first_name: string;
+  last_name: string;
+  GPA: number;
+  total_credits: number;
+}
 
 const StudentRoster = () => {
-  const [majors, setMajors] = useState(
-    // TODO: Get list of majors from database
-    TestMajors
+  const [studentRoster, setStudentRoster] = useState<RosterStudent[]>([]);
+
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  const fetchStudentRoster = async (
+    deptId: number
+  ): Promise<RosterStudent[]> => {
+    const response = await fetch(
+      `http://localhost:3000/api/summaries/studentRoster/${deptId}`
+    );
+
+    if (!response.ok)
+      throw new Error(`Failed to fetch course history: ${response.statusText}`);
+    return response.json();
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const deptId = sessionStorage.getItem("departmentId");
+        const roster = await fetchStudentRoster(Number(deptId));
+        setStudentRoster(roster);
+      } catch (err: any) {
+        console.error("Error fetching data:", err);
+        setError(err.message || "An unexpected error occurred.");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const majors = Array.from(
+    new Set(studentRoster.map((item) => item.major_name))
+  );
+  const studentsGrouped = majors.map((major) =>
+    studentRoster.filter((student) => student.major_name === major)
   );
 
-  const getStudentsByMajor = (major: string): Student[] => {
-    // TODO: query database for students by major
-    return TestStudents;
-  };
+  if (loading)
+    return (
+      <div className="spinner-border" role="status">
+        Loading...
+      </div>
+    );
+  if (error) return <div className="alert alert-danger">Error: {error}</div>;
 
   return (
     <>
@@ -36,15 +83,15 @@ const StudentRoster = () => {
                     </tr>
                   </thead>
                   <tbody>
-                    {getStudentsByMajor(major).map((student) => (
-                      <tr key={student.id}>
-                        <td>{student.id}</td>
+                    {studentsGrouped[index].map((student) => (
+                      <tr key={student.student_id}>
+                        <td>{student.student_id}</td>
                         <td>
-                          {student.firstName} {student.lastName}
+                          {student.first_name} {student.last_name}
                         </td>
 
-                        <td>{student.gpa}</td>
-                        <td>{student.totalCredits}</td>
+                        <td>{student.GPA}</td>
+                        <td>{student.total_credits}</td>
                       </tr>
                     ))}
                   </tbody>
